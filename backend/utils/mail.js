78dc1533035094d0660ed0961682c56b1047488f
Adarshjha09 <ak0632447@gmail.com>
2026-06-14 08:46:15 +0530

    debug: mail logger

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -9,6 +9,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL,
     pass: process.env.PASS,
   },
+  debug: true,
+  logger: true 
 });
 
 export const sendOtpMail=async (to,otp) => {
